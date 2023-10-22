5688c587a55ed9b89464a6270deed4c722e898b3
Bruno Silva <brunoelvis@outlook.com.br>
2023-10-22 13:16:27 -0300

    Extração da secret para o dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 import sign from "./jwt/sign";
 import verify from "./jwt/verify";
 
-const secret = 'epa';
+const secret = process.env.JWT_SECRET as string;
 
 const token = sign({
     exp: Date.now() + (24 * 60 * 60 * 1000),
